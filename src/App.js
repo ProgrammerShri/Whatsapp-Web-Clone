aa57490e8b996bd988af775b26408df3bc3bfa51
ProgrammerShri <shrikantdewangan3@gmail.com>
2022-10-08 01:17:34 +0530

    Loder Component Completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Loader from "./components/Loader";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
+  const { loading, value } = useProgress();
+
+  if (true) {
+    return <Loader value={value} />;
+  }
+
   return (
     <div className="App">
       <LandingPage />
@@ -10,3 +18,24 @@ function App() {
 }
 
 export default App;
+
+const useProgress = (maxTimeInSeconds = 300) => {
+  const [loading, setLoading] = useState(true);
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((oldVal) => {
+        const newVal = oldVal + 10;
+
+        if (newVal >= 100) {
+          clearInterval(interval);
+          setLoading(false);
+        }
+
+        return newVal;
+      });
+    }, 600);
+  }, []);
+
+  return { loading, value };
+};
---
+++ src/components/Loader.js
@@ -0,0 +1,33 @@
+import React from "react";
+import WhatsappIcon from "../assets/images/whatsapp-icon.png";
+
+const Loader = ({ value }) => {
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center text-gray-500">
+      <img src={WhatsappIcon} alt="logo" className="w-20 grayscale" />
+
+      <progress
+        className="progress w-96 h-0.5 my-10 bg-gray-300"
+        value={value}
+        max="100"
+      ></progress>
+
+      <p className="text-black py-1"> WhatsApp </p>
+      <span className="flex justify-center items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={16}
+          height={16}
+          fill="currentColor"
+          className="bi bi-lock-fill mx-2"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
+        </svg>
+        <p className="text-sm">End-to-end encrypted</p>
+      </span>
+    </div>
+  );
+};
+
+export default Loader;
